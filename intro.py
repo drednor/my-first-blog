6723a20e917dfdf44a120f60ae5efa111f0e3500
Abhishek Singh <singh.msoe@gmail.com>
2018-09-07 17:46:59 -0400

    My Django app, First commit

---
+++ intro.py
@@ -0,0 +1,19 @@
+print('abhishek')
+participant = {'name' : 'abhishek', 'age': '22', 'country' :'India'}
+print(participant)
+print(participant['age'])
+if 3<2:
+	print("hello")
+else:
+	print("no")
+#def hi(name):
+#	print("name " + name)
+
+print("done")
+#hi("abhishek")
+b = ["abhishel","sid","singh"]
+for name in b:
+	print(name)
+
+for i in range(1,11):
+	print(i)
